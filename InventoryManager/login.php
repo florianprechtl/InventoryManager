7cd8efb627502dc82980edf750e49fdd75b3108e
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-17 18:15:58 +0300

    updated login page to bootstrap theme

--- InventoryManager/login.php
+++ InventoryManager/login.php
@@ -5,7 +5,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/loginStyles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 
@@ -13,14 +14,32 @@
 </head>
 
 <body>
-    <p>Please enter your login data</p>
-    <form method="POST" action="checklogin.php" enctype="multipart/form-data">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <button type="submit" name="submit">Login</button>
-    </form>
+    <div class="login-container container-fluid">
+        <div class="row center padding-top">
+            <p class="col-sm-12">Please enter your login data</p>
+        </div>
+        <form method="POST" action="checklogin.php" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-sm-12 input-group input-group-sm mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-sm">Username</span>
+                    </div>
+                    <input type="text" class="form-control" name="username" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12 input-group input-group-sm mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-sm">Passwort</span>
+                    </div>
+                    <input type="text" class="form-control" name="password" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required>
+                </div>
+            </div>
+
+            <button class="btn btn-primary full-width margin-bottom" type="submit" name="submit">Login</button>
+        </form>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
