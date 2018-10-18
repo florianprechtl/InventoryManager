7359524729493f756cc51dc0c94a8e6820e1b586
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-18 14:30:10 +0300

    test for user validation

--- InventoryManager/checkLogin.php
+++ InventoryManager/checkLogin.php
@@ -8,24 +8,19 @@
     
     echo $username . " entered the password " . $password; 
     checkUserAccountInformation($username, $db);
-    echo "1";
 
     
     function checkUserAccountInformation($username, $db) {
-        echo "2";
-        $sql = "SELECT * FROM user";
+        $sql = "SELECT * FROM user WHERE Name = $username";
         
         $result = $db->query($sql);
-        echo "3";
         
         if ($result->num_rows > 0) {
-            echo "4";
             while($row = $result->fetch_assoc()) {
                 echo "userNr: " . $row['UserNr']. " - Name: " . $row['Name']. " " . $row['Password']. "<br>";
             }
         } else {
             echo "0 results";
         }
-        echo "5";
     }
 ?>
