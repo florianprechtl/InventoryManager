8cdb2193b683b6d449f1316edd73a6122cc95d3f
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-19 22:59:53 +0300

    added mockup inventory entry and aligned them with flex

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -16,9 +16,13 @@
 
 <body>
     <div class="container-fluid" style="max-width: 800px; width:100%">
+
+        <!--Header-->
         <div class="row header">
             <h1 class="col-sm-12" align="center">Inventory Manager</h1>
         </div>
+
+        <!--Search bar and Inventory select-->
         <div class="row justify-content-between margin-top">
             <div class="col-sm-5">
                 <div class="form-group">
@@ -35,7 +39,7 @@
             <div class="col-sm-5">
                 <label for="exampleFormControlSelect1">Search for specific entries:</label>
                 <div class="input-group" id="adv-search">
-                    
+
                     <input type="text" class="form-control" placeholder="Search for snippets" />
                     <div class="input-group-btn">
                         <div class="btn-group" role="group">
@@ -72,8 +76,27 @@
                 </div>
             </div>
         </div>
+
+        <!--Inventory Showplace-->
+        <div class="d-flex justify-content-around flex-wrap bd-highlight mb-3">
+            <div class="inventory-item-preview icon-box">
+                <a><i class="fas fa-plus"></i></a>
+            </div>
+            <div class="inventory-item-preview">
+            </div>
+            <div class="inventory-item-preview">
+            </div>
+            <div class="inventory-item-preview">
+            </div>
+            <div class="inventory-item-preview">
+            </div>
+            <div class="inventory-item-preview">
+            </div>
+            <div class="inventory-item-preview">
+            </div>
+        </div>
     </div>
-    
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
