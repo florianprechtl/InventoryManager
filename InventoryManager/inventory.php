76af12fc347bc74203ffa7167408dcdf7a13f2fe
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-21 19:54:51 +0300

    bugfix: no inventory selected

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -56,7 +56,11 @@
                                     }
                                 }
                             } else {
-                                echo "<option value='$row[InventoryNr]'>$row[Name]</option>";
+                                if ($result->num_rows > 0) {
+                                    while($row = $result->fetch_assoc()) {
+                                        echo "<option value='$row[InventoryNr]'>$row[Name]</option>";
+                                    }
+                                }
                             }
                         ?>
                         </select>
