ca17d878a8806b138712ebf20a86e9f17affaa01
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-22 12:59:06 +0300

    prodgrou insert test

--- InventoryManager/uploadInventoryentry.php
+++ InventoryManager/uploadInventoryentry.php
@@ -13,7 +13,7 @@
 
     if (trim($name_prod_gr) != '') {
         echo "here<br>";
-        $sql_prod_gr = "INSERT INTO productgroup (ProdgrNr, Name, Description, NameShort, IconName, IconExtension) VALUES (NULL, $name_prod_gr, $descr_prod_gr, NULL, NULL, NULL)";
+        $sql_prod_gr = "INSERT INTO productgroup (ProdgrNr, Name, Description, NameShort, IconName, IconExtension) VALUES (NULL, '$name_prod_gr', '$descr_prod_gr', NULL, NULL, NULL)";
         $result = mysqli_query($db, $sql_prod_gr) or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error($db), E_USER_ERROR);
         echo $result;
 //        if ($result->num_rows > 0) {
