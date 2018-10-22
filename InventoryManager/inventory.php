5305942b05d0eab06a51f01e7558ad8ba9c32bf5
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-22 10:18:14 +0300

    added date picker

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -11,15 +11,17 @@
     <link rel="stylesheet" href="styles/resetCss.css">
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/inventoryStyles.css">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
-    <link rel="stylesheet" href="croppie/croppie.css" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="croppie/croppie.css">
+    <link rel="stylesheet" href="bootstrap/datepicker/css/bootstrap-datepicker.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 
     <script src="jquery/jquery-3.3.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="bootstrap/js/bootstrap.js"></script>
     <script src="croppie/croppie.js"></script>
-
+    <script src="bootstrap/datepicker/js/bootstrap-datepicker.js"></script>
+    
     <title>Inventory Manager</title>
 </head>
 
@@ -172,6 +174,9 @@
                                                         <a class="col-sm-12 btn btn-success crop_image">Crop & Upload Image</a>
                                                     </div>
                                                 </div>
+                                                <div id="buying-date-container">
+                                                        <input type="text">
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -261,6 +266,15 @@
                         }
                     });
                 })
+            });
+
+            $('#buying-date-container input').datepicker({
+                format: "dd/mm/yyyy",
+                maxViewMode: 2,
+                todayBtn: "linked",
+                clearBtn: true,
+                todayHighlight: true,
+                toggleActive: true
             });
 
         });
