aef362d1aa599c49e4ce793e7295d974da4e35bd
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-10-23 15:30:02 +0300

    Update registermodal.php

--- InventoryManager/registermodal.php
+++ InventoryManager/registermodal.php
@@ -6,20 +6,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
-    <link rel="icon" href="../../favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="styles/resetCss.css">
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/inventoryStyles.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="croppie/croppie.css">
     <link rel="stylesheet" href="bootstrap/datepicker/css/bootstrap-datepicker.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 
     <script src="jquery/jquery-3.3.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="bootstrap/js/bootstrap.js"></script>
-    <script src="croppie/croppie.js"></script>
     <script src="bootstrap/datepicker/js/bootstrap-datepicker.js"></script>
 
     <title>Register Modal</title>
@@ -56,21 +52,8 @@
             <?php
                $db = connectToDB();
         
-                if (isset($_GET["inventory"])) {
-                    $inventoryNr = $_GET["inventory"];
-                    $sql = "SELECT * FROM inventoryentry inner join product on inventoryentry.ProductNr = product.ProdNr where InventoryNr = $inventoryNr";
-                } else {
-                    $sql = "SELECT * FROM inventoryentry inner join product on inventoryentry.ProductNr = product.ProdNr where InventoryNr = 1;";
-                }
-        
-                $result = $db->query($sql);
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        echo "  <div class='inventory-item-preview'>
-                                    $row[Name]
-                                </div>";
-                    }
-                }
+                
+                 
             ?>
         </div>
 </div>
