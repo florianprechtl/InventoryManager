dd27a4675a9e782fc0f1273746048b0cc980ca0b
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-10-23 16:22:16 +0300

    Update registermodal.php

--- InventoryManager/registermodal.php
+++ InventoryManager/registermodal.php
@@ -26,7 +26,7 @@
 
 	
  <div class="padding-bottom" >
-                <a class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter" >Not registered yet? Click here!</a>
+                <a class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter" style="color:white" >Not registered yet? Click here!</a>
 </div>
 
 
