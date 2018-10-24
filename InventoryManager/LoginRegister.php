830920f52901916df63157d47ce6649397cf9edf
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-10-24 10:14:53 +0300

    Update LoginRegister.php

--- InventoryManager/LoginRegister.php
+++ InventoryManager/LoginRegister.php
@@ -64,7 +64,12 @@
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-	    <p> Hey </p>
+	  
+	    if ($username == NULL){
+	    <p> This username has already an account </p>
+	    }
+	    
+	else {    
       <div class="modal-header">
         <h5 class="modal-title" style="color:blue" id="exampleModalCenterTitle">REGISTRATION INFO</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -117,7 +122,7 @@
       </div>
     </div>
  
-
+}
         </div>
 </div>
 
