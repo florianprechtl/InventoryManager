16ed04285315f3d3c66c4a875bba9ccbaca858e4
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-10-24 12:25:44 +0300

    Update LoginRegister.php

--- InventoryManager/LoginRegister.php
+++ InventoryManager/LoginRegister.php
@@ -110,7 +110,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">REGISTER</button>
+	<button type="submit" name="submit" class="btn btn-primary">REGISTER</button>
       </div>
     </div>
  </form>
