7255aa6201cfc2ec98bf00742f2bea5a910cc36e
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-10-24 14:15:54 +0300

    Update registermodal.php

--- InventoryManager/registermodal.php
+++ InventoryManager/registermodal.php
@@ -6,6 +6,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="styles/resetCss.css">
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/inventoryStyles.css">
