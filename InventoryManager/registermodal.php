e470f294e0edce1070d250a2bdde28d34629cb2d
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-10-24 14:11:45 +0300

    Update registermodal.php

--- InventoryManager/registermodal.php
+++ InventoryManager/registermodal.php
@@ -22,11 +22,13 @@
 </head>
 
     <!-- Button trigger modal -->
-<div class="container-fluid" style="max-width: 800px; width:100%">
+<div class="login-container container-fluid">
+        <!--Header-->
+        <div class="row header">
+            <h1 class="col-sm-12" align="center">MyFridge</h1>
+	</div>
 	
- <div class="row center padding-top">
-	 <h1 class="col-sm-12" align="center">MyFridge</h1>
-</div>
+
     <br>
     <br>
 
@@ -86,6 +88,7 @@
 
         </form>
     </div>
+
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
