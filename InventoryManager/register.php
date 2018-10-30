df8d4f3206a2ef9130eed8d86b1ad0e989ce9221
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-30 14:48:25 +0200

    some code style fixes

--- InventoryManager/register.php
+++ InventoryManager/register.php
@@ -1,20 +1,20 @@
- 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-	    <!-- Required meta tags -->
-     		<meta charset="utf-8">
-   		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
- 	    <!-- Bootstrap CSS -->
-   		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
- 	 <title>Registration for new users </title>
-  	 <style>
-		body {
+    <title>Registration for new users </title>
+    <style>
+        body {
 			background-color: white; }
 		h1 { color : blue;
 		     text-align : center;
@@ -32,57 +32,63 @@
 	            text-align : center;
 	            /*background-color : yellow;*/}
 
-	 </style>	 
+	 </style>
 
 
 </head>
 
 <body>
 
-  <h1 style="margin:auto; width:600px;height:120px;border:10px solid lightblue;"> REGISTRATION PAGE</h1>
+    <h1 style="margin:auto; width:600px;height:120px;border:10px solid lightblue;"> REGISTRATION PAGE</h1>
 
-    <p class ="info">Give those information to register</p>
-    
-	<form>
+    <p class="info">Give those information to register</p>
 
-    <p class = "normal"> Name : 
-    <input type="text" name="name" required> <p>
+    <form>
 
-    <p class = "normal"> Family name : 
-    <input type="text" name="family" required> <p>
+        <p class="normal"> Name :
+            <input type="text" name="name" required>
+        </p>
 
-    <p class = "normal"> Age :
-    <input type="age" name="age" required> <p>
+        <p class="normal"> Family name :
+            <input type="text" name="family" required>
+        </p>
 
-     <div class="form-group" style="margin:auto; width:250px;height:140px; color : blue; text-align : center;">
-    <label for="exampleFormControlSelect2"> Choose a gender: </label>
-  	  <select multiple class="form-control" id="exampleFormControlSelect2">
-    	 	 <option>M</option>
-     		 <option>F</option>
-      		 <option>Other</option>
-     	 </select>
-    </div>
+        <p class="normal"> Age :
+            <input type="age" name="age" required>
+        </p>
+
+        <div class="form-group" style="margin:auto; width:250px;height:140px; color : blue; text-align : center;">
+            <label for="exampleFormControlSelect2"> Choose a gender: </label>
+            <select multiple class="form-control" id="exampleFormControlSelect2">
+                <option>M</option>
+                <option>F</option>
+                <option>Other</option>
+            </select>
+        </div>
 
 
-    <p class = "normal"> Email :
-    <input type="text" name="email" required> <p>
+        <p class="normal"> Email :
+            <input type="text" name="email" required>
+        </p>
 
-    <p class = "important"> User name : 
-    <input type="text" name="username" required> <p>
+        <p class="important"> User name :
+            <input type="text" name="username" required>
+        </p>
 
-    <p class = "important"> Password :
-    <input type="password" placeholder="Choose a password" name="psw" required> <p>
-	
-    <p class = "important"> Repeat Password :
-    <input type="password" placeholder="Repeat password" name="repeatedpsw" required> <p>
+        <p class="important"> Password :
+            <input type="password" placeholder="Choose a password" name="psw" required>
+        </p>
 
-    <button type="button" class="btn btn-primary btn-lg btn-block" style="margin:auto; width:300px;height:100px;">SEND REGISTRATION</button>
+        <p class="important"> Repeat Password :
+            <input type="password" placeholder="Repeat password" name="repeatedpsw" required>
+        </p>
 
-    
-    
+        <button type="button" class="btn btn-primary btn-lg btn-block" style="margin:auto; width:300px;height:100px;">SEND REGISTRATION</button>
 
-		</form>
+
+
+
+    </form>
 </body>
+
 </html>
-
-
--- InventoryManager/registermodal.php
+++ InventoryManager/registermodal.php
@@ -22,72 +22,77 @@
     <title>Register Modal</title>
 </head>
 
+<body>
     <!-- Button trigger modal -->
-<div class="login-container container-fluid">
+    <div class="login-container container-fluid">
         <!--Header-->
         <div class="row header">
             <h1 class="col-sm-12" align="center">MyFridge</h1>
-	</div>
-	
-
-    <br>
-    <br>
-
-	
- <div class="padding-bottom" >
-                <a class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModalCenter" style="color:white" >REGISTER NOW</a>
- </div>
-</div>
-		
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">    	     
-      <div class="modal-header">
-        <h5 class="modal-title" style="color:blue" id="exampleModalCenterTitle">REGISTRATION DEMAND</h5>
-        <button type="button" class="close"  onclick="myFunction()" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="mx-auto">
-          
-        <form method="POST" action="uploaduser.php" enctype="multipart/form-data">
-
-    <p> First Name : 
-    <input type="text" name="firstname" class="form-control"  aria-label="Small" aria-describedby="inputGroup-sizing-sm"  > <p>
-
-    <p> Last name : 
-    <input type="text" name="lastname" class="form-control"  aria-label="Small" aria-describedby="inputGroup-sizing-sm" > <p>
-
-    		
- 
-		
-    <p> Age :
-    <input type="text" name="age"  class="form-control"  aria-label="Small" aria-describedby="inputGroup-sizing-sm" > <p>
-	   
-    <p> User name : 
-    <input type="text" name="newusername" class="form-control"aria-label="Small" aria-describedby="inputGroup-sizing-sm" > <p>
-    <p> Password :
-    <input type="password" placeholder="Choose a password" name="psw" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" > <p>
-	
-    <p> Repeat Password :
-    <input type="password" placeholder="Repeat password" name="repeatedpsw" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" > <p>
-          
-          
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-	<button type="submit" name="submit" class="btn btn-primary">REGISTER</button>
-      </div>
-    </div>
- </form>
-	
         </div>
-</div>
 
 
-        </form>
+        <br>
+        <br>
+
+
+        <div class="padding-bottom">
+            <a class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModalCenter" style="color:white">REGISTER NOW</a>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" style="color:blue" id="exampleModalCenterTitle">REGISTRATION DEMAND</h5>
+                    <button type="button" class="close" onclick="myFunction()" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                
+                <form method="POST" action="uploaduser.php" enctype="multipart/form-data">
+                    <div class="modal-body">
+                        <div class="mx-auto">
+
+
+                            <p> First Name :
+                                <input type="text" name="firstname" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                            </p>
+
+                            <p> Last name :
+                                <input type="text" name="lastname" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                            </p>
+
+
+
+
+                            <p> Age :
+                                <input type="text" name="age" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                            </p>
+
+                            <p> User name :
+                                <input type="text" name="newusername" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                            </p>
+                            <p> Password :
+                                <input type="password" placeholder="Choose a password" name="psw" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                            </p>
+
+                            <p> Repeat Password :
+                                <input type="password" placeholder="Repeat password" name="repeatedpsw" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                            </p>
+
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" name="submit" class="btn btn-primary">REGISTER</button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
     </div>
 
 
@@ -98,4 +103,3 @@
 
 
 </html>
-
