d01d5d74433e556052fed1343c209f7b82556d12
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-30 18:13:27 +0200

    typo

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -331,7 +331,7 @@
             });
             
             $('.button-remove').click(function(event) {
-                $ajax({
+                $.ajax({
                     url: 'removeInventoryEntry.php',
                     type: "POST",
                     dataType: 'json',
