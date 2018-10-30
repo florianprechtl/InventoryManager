ab9c8b7c7da81d08fba41ab40e7278a61002ecb9
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-30 17:37:41 +0200

    added removeButton for InventoryEntries

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -255,7 +255,7 @@
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         echo "  <div class='inventory-item-preview'>"
-                        echo "  <a type="button" class='button-remove' id='button_remove_$row[InventoryNr]'></a>"
+                        echo "  <a type="button" class='button-remove' id='button_remove_$row[InventoryNr]'></a>";
                         echo "  $row[Name]
                                 <br>
                                 <br>
