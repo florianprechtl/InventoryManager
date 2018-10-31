fa6677c77f1487c2a4fe2db222ddf4a7c52ea151
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-31 22:57:38 +0200

    commenting and refactoring of uploadInventoryEntry.php

--- InventoryManager/uploadInventoryentry.php
+++ InventoryManager/uploadInventoryentry.php
@@ -4,24 +4,35 @@
 
     $db = connectToDB();
 
+    // variables of the product
     $name_product = $_POST['name_product'];
     $descr_product = $_POST['descr_product'];
     $unit = $_POST['unit'];
+    $imageBase64 = $_POST['imageBase64'];
+
+    //variables of the inventoryentry
     $amount = $_POST['amount'];
     $date_buying = convertDate($_POST['date_buying']);
     $date_expiring = convertDate($_POST['date_expiring']);
     $inventory = $_GET['inventory'];
-    $imageBase64 = $_POST['imageBase64'];
+
+    if (isset($_POST['submit'])) {
+        $productNr = insertProduct($db, $name_product, $descr_product, $unit, $imageBase64);
+        insertInventoryEntry($db, $inventory, $productNr, null, $amount, $date_buying, $date_expiring, null);
+        redirect('inventory.php?inventory='.$inventory);
+    }
+    
     
 
+    /*
+    * function that inserts product and returns the prodNr of the inserted product
+    */
     function insertProduct($db, $name, $descr, $unit, $imageBase64) {
         
         // set values of varaibles
         $name = $name != '' ? $name : null;
         $descr = $descr != '' ? $descr : null;
         $unit = $unit != '' ? $unit : null;
-        
-        
         $imageName = null;
         $data = null;
         $imageName = time() . '.png';
@@ -29,6 +40,7 @@
         
         $productNr = null;
         
+        // get next autoincrement value that is returned by the function
         $sql = "SHOW TABLE STATUS WHERE name='product'";
         $result = $db->query($sql);
         if ($result->num_rows > 0) {
@@ -37,17 +49,23 @@
             }
         }
         
+        // insert product 
         $sql = "INSERT INTO product (ProdNr, ProdgrNr, Name, Description, Unit, Image) VALUES (NULL, NULL, '$name', '$descr', '$unit', '$imageName')";
         echo $sql;
         $db->query($sql);
 
+        // upload image to the "imgUploads" folder
         file_put_contents("imgUploads/$imageName", $data); 
         return $productNr;
     }
 
+
+    /*
+    *   Function that inserts inventoryentry
+    */
     function insertInventoryEntry($db, $inventoryNr, $productNr, $userNr, $amount, $buyingDate, $expiringDate, $status) {
         
-        // set values of varaibles
+        // set values of variables
         $inventoryNr = $inventoryNr != '' ? $inventoryNr : 'null';
         $productNr = $productNr != '' ? $productNr : 5;
         $userNr = $userNr != '' ? $userNr : 1;
@@ -56,65 +74,10 @@
         $expiringDate = $expiringDate != '' ? "'$expiringDate'" : 'null';
         $status = $status != '' ? $status : 'null';
         
-        
+        // insert inventor entry
         $sql = "INSERT INTO inventoryentry (InventoryEntryNr, InventoryNr, ProductNr, UserNr, Amount, BuyingDate, ExpiringDate, Status) 
                                 VALUES (NULL, $inventoryNr, $productNr, $userNr, $amount, $buyingDate, $expiringDate, $status)"; 
         echo $sql;
         $db->query($sql);
     }
-
-    if (isset($_POST['submit'])) {
-        $productNr = insertProduct($db, $name_product, $descr_product, $unit, $imageBase64);
-        insertInventoryEntry($db, $inventory, $productNr, null, $amount, $date_buying, $date_expiring, null);
-//        redirect('inventory.php?inventory='.$inventory);
-    }
-
-//        $sql = "INSERT INTO files(mime,data) VALUES(:mime,:data)";
-//        $stmt = $db->prepare($sql);
-//
-//        $stmt->bindParam(':mime', $mime);
-//        $stmt->bindParam(':data', $blob, PDO::PARAM_LOB);
-//
-//        return $stmt->execute();
-//    }
-
-//    if (trim($name_prod_gr) != '') {
-//        
-//        //works but we should not do it that way
-//        $sql_prod_gr = "INSERT INTO productgroup (ProdgrNr, Name, Description, NameShort, IconName, IconExtension) VALUES (NULL, '$name_prod_gr', '$descr_prod_gr', NULL, NULL, NULL)";
-//        $db->query($sql_prod_gr);   
-//    }
-//
-//    if (trim($name_product) != '') {
-//        // works as well, but we shoud not do it that way
-//        $sql_product = "INSERT INTO product (ProdNr, ProdgrNr, Name, Description, Unit, ImageName, ImageExtension) VALUES (NULL, NULL, '$name_product', '$descr_product', '$unit', NULL, NULL)"
-//        $db->query($sql_product); 
-//    }
-        // works as well, but we shoud not do it that way
-        
-
-//        redirect('inventory.php?inventory='.$inventory);
-
-
-//    $sql = "SELECT * FROM Inventory";
-//
-//    $result = $db->query($sql);
-//
-//    if (isset($_GET["inventory"])) {
-//        if ($result->num_rows > 0) {
-//            while($row = $result->fetch_assoc()) {
-//                if ($_GET['inventory'] == $row[InventoryNr]) {
-//                    echo "<option value='$row[InventoryNr]'  selected>$row[Name]</option>";
-//                } else {
-//                    echo "<option value='$row[InventoryNr]'>$row[Name]</option>";
-//                }
-//            }
-//        }
-//    } else {
-//        if ($result->num_rows > 0) {
-//            while($row = $result->fetch_assoc()) {
-//                echo "<option value='$row[InventoryNr]'>$row[Name]</option>";
-//            }
-//        }
-//    }
 ?>
