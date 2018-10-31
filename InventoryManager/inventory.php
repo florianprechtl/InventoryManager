42f19924270f4fe771fd77a894844d9318c976ba
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-31 23:28:58 +0200

    show uploaded image

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -344,7 +344,7 @@
                     while($row = $result->fetch_assoc()) {
                         $img = file_get_contents("imgUploads/$row[Image]");
                         
-                        echo "<div class='inventory-item-preview' style=background-image:url(imgUploads/" . $row['Image'] . ");></div>";
+                        echo "<div class='inventory-item-preview' style=background-image:url(imgUploads/" . $row['Image'] . ");>";
                         echo "<i class='btn btn-danger button-remove' id='button_remove_$row[InventoryEntryNr]'>X</i>";
                         echo "$row[Name]<br><br>$row[InventoryEntryNr]</div>";
                     }
