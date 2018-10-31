1fd36c535091c8693703d708c5dce1529c151d13
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-10-31 13:56:18 +0200

    taking a look at the structure of blob

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -447,7 +447,7 @@
                 var image = new Image();
                 image.src = 'data:image/png;' + input[0] + ',' + input[1];
                 
-                document.inventoryEntryForm.blob.value = dataURItoBlob(image.src);
+                document.forms['inventoryEntryForm'].elements['blob'].value = dataURItoBlob(image.src);
                 
                 $('#imagePreview').html(image);
                 $('#imagePreview').css("width", "100%");
