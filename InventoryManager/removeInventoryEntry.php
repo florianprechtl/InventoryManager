dab8636ee4202f8a4c8b20f5e017708253ac57ca
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-01 19:13:14 +0200

    delete img on server when entry is deleted

--- InventoryManager/removeInventoryEntry.php
+++ InventoryManager/removeInventoryEntry.php
@@ -15,7 +15,7 @@
     $file = "imgUploads/.$_POST[imgName]";
     $ftp_server = "ws-prod-am2-223.ftp.azurewebsites.windows.net";
     $ftp_user_name = "1801674PHP\user1801674";
-    $ftp_user_pass = "passwort";
+    $ftp_user_pass = "Blumenbeet1";
 
     // connect to ftp
     $conn_id = ftp_connect($ftp_server);
