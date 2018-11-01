b0f5933da422c4f849e22e2f2aa17c99cbdf523f
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-01 19:32:47 +0200

    delete img on server when entry is deleted

--- InventoryManager/removeInventoryEntry.php
+++ InventoryManager/removeInventoryEntry.php
@@ -13,20 +13,20 @@
 
     // variables
     $file = "imgUploads/$_POST[imgName]";
-    $ftp_server = "ftp://waws-prod-am2-223.ftp.azurewebsites.windows.net";
-    $ftp_user_name = "1801674PHP\user1801674";
+    $ftp_server = 'ftp://waws-prod-am2-223.ftp.azurewebsites.windows.net';
+    $ftp_user_name = '1801674PHP\user1801674';
     $ftp_user_pass = "Blumenbeet1";
 
     // connect to ftp
-    $conn_id = ftp_connect($ftp_server);
+    $conn_id = ftp_connect($ftp_server);        
 
     // login with username and password
     $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
 
     // try to delete file
-    if (ftp_delete($conn_id, $file)) {
-     echo "$file deleted successful\n";
-    } else {
-     echo "could not delete $file\n";
-    }
+//    if (ftp_delete($conn_id, $file)) {
+//     echo "$file deleted successful\n";
+//    } else {
+//     echo "could not delete $file\n";
+//    }
 ?>
