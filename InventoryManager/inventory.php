830cad7fa4cdfeb9fa92beb41147602460f950cf
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-05 16:23:43 +0200

    adapted filename references

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -276,7 +276,7 @@
                                                                     $result = $db->query($sql);
                                                                     if ($result->num_rows > 0) {
                                                                         while ($row = $result->fetch_assoc()) {
-
+                                                                            array_push($products, new Product($_POST['Name'], $_POST['Description'], $_POST['Image']));
                                                                             echo "<option value='$row[ProductNr]'>$row[Name]</option>";
                                                                         }
                                                                     }
