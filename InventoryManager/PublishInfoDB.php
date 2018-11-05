ec52c4478b03b5a7dbc06be235164cba3e0a5048
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-11-05 11:44:07 +0200

    Create PublishInfoDB.php

---
+++ InventoryManager/PublishInfoDB.php
@@ -0,0 +1,26 @@
+<?php
+	include("connectDB.php");
+	
+    $db = connectToDB();
+  	$variablename = $_POST['variablename'];
+  
+    
+    echo $variablename . " c est le nom qui sert de variable " ; 
+
+    
+    function PublishInfo($variablename, $db) {
+        $sql = "SELECT * FROM product WHERE Name = '$variablename'";
+        
+        $result = $db->query($sql);
+        
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {      
+                    echo "<b>$oui</b> " . $row['UserNr'].                       
+                        "<br>";
+                    echo "<div class='padding-bottom'><a href='inventory.php'>Go to Main Page</a></div>";            
+            }
+        } else {
+            echo "There is no name: <b>$username</b><br>";
+        }
+    }
+?>
