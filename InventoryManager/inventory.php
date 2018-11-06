0e40266af785eeaef39dab349ea8922bebb0476d
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-06 19:35:23 +0200

    outsource unnecessary scripts and styles

--- InventoryManager/inventory.php
+++ InventoryManager/inventory.php
@@ -22,84 +22,10 @@
     <script src="bootstrap/js/bootstrap.js"></script>
     <script src="croppie/croppie.js"></script>
     <script src="bootstrap/datepicker/js/bootstrap-datepicker.js"></script>
+    <script src="scripts/imgCrop.js"></script>
 
     <title>Inventory Manager</title>
 </head>
-
-<style>
-
-    .avatar-upload {
-        position: relative;
-        max-width: 209px;
-        margin: auto;
-    }
-
-    .avatar-upload .avatar-edit {
-        position: absolute;
-        right: -10px;
-        z-index: 1;
-        top: 176px;
-        text-align: center;
-    }
-
-    .avatar-upload .avatar-edit input {
-        display: none;
-    }
-
-    .avatar-upload .avatar-edit input + label {
-        display: inline-block;
-        width: 45px;
-        height: 45px;
-        margin-bottom: 0;
-        background: #031D54;
-        color: #FFFFFF;
-        border: 1px solid #031D54;
-        border-radius: 100%;
-        box-shadow: 0 0 5px 0 #031D54;
-        cursor: pointer;
-        font-weight: normal;
-        transition: all 0.2s ease-in-out;
-    }
-
-    .avatar-upload .avatar-edit input + label:hover {
-        background: #B1C7F5;
-        border: 3px solid #031D54;
-        color: #031D54;
-        box-shadow: none;
-    }
-
-    .avatar-upload .avatar-edit input + label:after {
-        color: #757575;
-        position: absolute;
-        top: 10px;
-        left: 0;
-        right: 0;
-        text-align: center;
-        margin: auto;
-    }
-
-    .avatar-upload .avatar-preview {
-        width: 209px;
-        height: 209px;
-        position: relative;
-        border: 5px solid #031D54;
-        border-radius: 5px;
-        overflow: hidden;
-        box-shadow: 0 0 5px 2px #B1C7F5;
-    }
-
-    .avatar-upload .avatar-preview > div {
-        width: 100%;
-        height: 100%;
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-    }
-    
-    #image_upload_icon {
-        line-height: 45px;
-    }
-</style>
 
 <body>
     <div class="container-fluid" style="max-width: 800px; width:100%">
@@ -365,109 +291,109 @@
         </div>
     </div>
 
-    <script>
-        $(document).ready(function() {
-
-            $('#image_demo').fadeOut();
-
-            $image_crop = $('#image_demo').croppie({
-                enableExif: true,
-                enableRotate: true,
-                enableOrientation: true,
-                viewport: {
-                    width: 200,
-                    height: 200,
-                    type: 'square', //circle
-                },
-                boundary: {
-                    width: 300,
-                    height: 300
-                }
-            });
-
-            $('#upload_image').on('change', function() {
-                var reader = new FileReader();
-                reader.onload = function(event) {
-                    $image_crop.croppie('bind', {
-                        url: event.target.result
-                    }).then(function() {
-                        console.log('jQuery bind complete');
-                    });
-                };
-                reader.readAsDataURL(this.files[0]);
-                $('#image_demo').fadeIn();
-                $('#button-upload-pic').fadeIn();
-                $('#image_preview_container').fadeOut();
-            });
-
-            $('#button_crop_image').click(function() {
-                $image_crop.croppie('result', {
-                    type: 'canvas',
-                    size: 'viewport'
-                }).then(function(response) {
-                    $image_array_1 = response.split(";");
-                    $image_array_2 = $image_array_1[1].split(",");
-
-                    $('#image_demo').fadeOut();
-                    $('#button-upload-pic').fadeOut();
-                    $('#image_preview_container').fadeIn();
-                    preshowPicture($image_array_2);
-                })
-            });
-
-            $('#buying_date_container input').datepicker({
-                format: "dd/mm/yyyy",
-                maxViewMode: 2,
-                todayBtn: "linked",
-                clearBtn: true,
-                todayHighlight: true,
-                toggleActive: true
-            });
-
-            $('.button-remove').click(function(event) {
-                $.ajax({
-                    url: 'removeInventoryEntry.php',
-                    type: "POST",
-                    data: {
-                        // the number of the removed button, shown in the id
-                        'nr': event.target.id.split('_')[2],
-                        'imgName': event.target.id.split('_')[3]
-                    },
-                    success: function() {
-                        location.reload();
-                    },
-                    error: function() {
-                        alert("Removing inventory entry did not work!");
-                    }
-                });
-            });
-
-            $('#button_fade_to_new_product').click(function() {
-                $('#content_existing_product').slideUp(750, function() {
-                    $('#content_new_product').slideDown(750);
-                })
-            });
-
-            $('#button_fade_to_existing_product').click(function() {
-                $('#content_new_product').slideUp(750, function() {
-                    $('#content_existing_product').slideDown(750);
-                })
-            });
-
-            function preshowPicture(input) {
-                var image = new Image();
-                image.src = 'data:image/png;' + input[0] + ',' + input[1];
-                
-                $imageBase64 = input[1];
-                document.forms['inventoryEntryForm'].elements['imageBase64'].value = $imageBase64;
-                
-                $('#imagePreview').html(image)
-                    .css("width", "100%")
-                    .hide().fadeIn(650);
-            }
-        });
-
-    </script>
+<!--    <script>-->
+<!--        $(document).ready(function() {-->
+<!---->
+<!--            $('#image_demo').fadeOut();-->
+<!---->
+<!--            $image_crop = $('#image_demo').croppie({-->
+<!--                enableExif: true,-->
+<!--                enableRotate: true,-->
+<!--                enableOrientation: true,-->
+<!--                viewport: {-->
+<!--                    width: 200,-->
+<!--                    height: 200,-->
+<!--                    type: 'square', //circle-->
+<!--                },-->
+<!--                boundary: {-->
+<!--                    width: 300,-->
+<!--                    height: 300-->
+<!--                }-->
+<!--            });-->
+<!---->
+<!--            $('#upload_image').on('change', function() {-->
+<!--                var reader = new FileReader();-->
+<!--                reader.onload = function(event) {-->
+<!--                    $image_crop.croppie('bind', {-->
+<!--                        url: event.target.result-->
+<!--                    }).then(function() {-->
+<!--                        console.log('jQuery bind complete');-->
+<!--                    });-->
+<!--                };-->
+<!--                reader.readAsDataURL(this.files[0]);-->
+<!--                $('#image_demo').fadeIn();-->
+<!--                $('#button-upload-pic').fadeIn();-->
+<!--                $('#image_preview_container').fadeOut();-->
+<!--            });-->
+<!---->
+<!--            $('#button_crop_image').click(function() {-->
+<!--                $image_crop.croppie('result', {-->
+<!--                    type: 'canvas',-->
+<!--                    size: 'viewport'-->
+<!--                }).then(function(response) {-->
+<!--                    $image_array_1 = response.split(";");-->
+<!--                    $image_array_2 = $image_array_1[1].split(",");-->
+<!---->
+<!--                    $('#image_demo').fadeOut();-->
+<!--                    $('#button-upload-pic').fadeOut();-->
+<!--                    $('#image_preview_container').fadeIn();-->
+<!--                    preshowPicture($image_array_2);-->
+<!--                })-->
+<!--            });-->
+<!---->
+<!--            $('#buying_date_container input').datepicker({-->
+<!--                format: "dd/mm/yyyy",-->
+<!--                maxViewMode: 2,-->
+<!--                todayBtn: "linked",-->
+<!--                clearBtn: true,-->
+<!--                todayHighlight: true,-->
+<!--                toggleActive: true-->
+<!--            });-->
+<!---->
+<!--            $('.button-remove').click(function(event) {-->
+<!--                $.ajax({-->
+<!--                    url: 'removeInventoryEntry.php',-->
+<!--                    type: "POST",-->
+<!--                    data: {-->
+<!--                        // the number of the removed button, shown in the id-->
+<!--                        'nr': event.target.id.split('_')[2],-->
+<!--                        'imgName': event.target.id.split('_')[3]-->
+<!--                    },-->
+<!--                    success: function() {-->
+<!--                        location.reload();-->
+<!--                    },-->
+<!--                    error: function() {-->
+<!--                        alert("Removing inventory entry did not work!");-->
+<!--                    }-->
+<!--                });-->
+<!--            });-->
+<!---->
+<!--            $('#button_fade_to_new_product').click(function() {-->
+<!--                $('#content_existing_product').slideUp(750, function() {-->
+<!--                    $('#content_new_product').slideDown(750);-->
+<!--                })-->
+<!--            });-->
+<!---->
+<!--            $('#button_fade_to_existing_product').click(function() {-->
+<!--                $('#content_new_product').slideUp(750, function() {-->
+<!--                    $('#content_existing_product').slideDown(750);-->
+<!--                })-->
+<!--            });-->
+<!---->
+<!--            function preshowPicture(input) {-->
+<!--                var image = new Image();-->
+<!--                image.src = 'data:image/png;' + input[0] + ',' + input[1];-->
+<!--                -->
+<!--                $imageBase64 = input[1];-->
+<!--                document.forms['inventoryEntryForm'].elements['imageBase64'].value = $imageBase64;-->
+<!--                -->
+<!--                $('#imagePreview').html(image)-->
+<!--                    .css("width", "100%")-->
+<!--                    .hide().fadeIn(650);-->
+<!--            }-->
+<!--        });-->
+<!---->
+<!--    </script>-->
 
 </body>
 
---
+++ InventoryManager/scripts/imgCrop.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+    $('#image_demo').fadeOut();
+
+    $image_crop = $('#image_demo').croppie({
+        enableExif: true,
+        enableRotate: true,
+        enableOrientation: true,
+        viewport: {
+            width: 200,
+            height: 200,
+            type: 'square', //circle
+        },
+        boundary: {
+            width: 300,
+            height: 300
+        }
+    });
+
+    $('#upload_image').on('change', function() {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            $image_crop.croppie('bind', {
+                url: event.target.result
+            }).then(function() {
+                console.log('jQuery bind complete');
+            });
+        };
+        reader.readAsDataURL(this.files[0]);
+        $('#image_demo').fadeIn();
+        $('#button-upload-pic').fadeIn();
+        $('#image_preview_container').fadeOut();
+    });
+
+    $('#button_crop_image').click(function() {
+        $image_crop.croppie('result', {
+            type: 'canvas',
+            size: 'viewport'
+        }).then(function(response) {
+            $image_array_1 = response.split(";");
+            $image_array_2 = $image_array_1[1].split(",");
+
+            $('#image_demo').fadeOut();
+            $('#button-upload-pic').fadeOut();
+            $('#image_preview_container').fadeIn();
+            preshowPicture($image_array_2);
+        })
+    });
+
+    $('#buying_date_container input').datepicker({
+        format: "dd/mm/yyyy",
+        maxViewMode: 2,
+        todayBtn: "linked",
+        clearBtn: true,
+        todayHighlight: true,
+        toggleActive: true
+    });
+
+    $('.button-remove').click(function(event) {
+        $.ajax({
+            url: 'removeInventoryEntry.php',
+            type: "POST",
+            data: {
+                // the number of the removed button, shown in the id
+                'nr': event.target.id.split('_')[2],
+                'imgName': event.target.id.split('_')[3]
+            },
+            success: function() {
+                location.reload();
+            },
+            error: function() {
+                alert("Removing inventory entry did not work!");
+            }
+        });
+    });
+
+    $('#button_fade_to_new_product').click(function() {
+        $('#content_existing_product').slideUp(750, function() {
+            $('#content_new_product').slideDown(750);
+        })
+    });
+
+    $('#button_fade_to_existing_product').click(function() {
+        $('#content_new_product').slideUp(750, function() {
+            $('#content_existing_product').slideDown(750);
+        })
+    });
+
+    function preshowPicture(input) {
+        var image = new Image();
+        image.src = 'data:image/png;' + input[0] + ',' + input[1];
+
+        $imageBase64 = input[1];
+        document.forms['inventoryEntryForm'].elements['imageBase64'].value = $imageBase64;
+
+        $('#imagePreview').html(image)
+            .css("width", "100%")
+            .hide().fadeIn(650);
+    }
+});
