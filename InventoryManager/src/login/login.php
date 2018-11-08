934f2e347775c114df44d663ba704901810f4916
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-08 22:36:20 +0200

    clean code

--- InventoryManager/src/login/login.php
+++ InventoryManager/src/login/login.php
@@ -1,80 +1,69 @@
 <!-- This code is for the login page and permits the access to the registration modal -->
 
-<?php
-    include('login-register/registerNewUser_Modal.php');
-?>
 <html>
+    <?php
+        include('login-register/registerNewUser_Modal.php');
+        include('../common/header_session_start.php');
+    ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="../../styles/resetCss.css">
-    <link rel="stylesheet" href="../../styles/styles.css">
-    <link rel="stylesheet" href="../../styles/loginStyles.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <body>
+        <div class="login-container container-fluid">
 
+            <!-- Site header -->
+            <div class="row header">
+                <h1 class="col-sm-12" align="center">MyFridge - Login</h1>
+            </div>
 
-    <title>Inventory Manager Login</title>
-</head>
+            <!-- Form header -->
+            <div class="row center padding-top">
+                <p class="col-sm-12">Please enter your login data</p>
+            </div>
 
-<body>
-    <div class="login-container container-fluid">
-        <!--Header-->
-        <div class="row header">
-            <h1 class="col-sm-12" align="center">MyFridge - Login</h1>
-        </div>
-        <div class="row center padding-top">
-            <p class="col-sm-12">Please enter your login data</p>
-        </div>
-        <?php
-        if (isset($_GET['loginDenied'])) {
-            if ($_GET['loginDenied'] == 'userUnknown') {
-                echo "  <div class='alert alert-danger' role='alert'>
-                                User does not exist!
-                            </div>";
-            } else if ($_GET['loginDenied'] == 'wrongPassword') {
-                echo "  <div class='alert alert-danger' role='alert'>
-                                Wrong password!
-                            </div>";
-            }
-        }
-        if (isset($_GET['registerSuccessful'])) {
-            if ($_GET['registerSuccessful'] == 'false') {
-                echo "  <div class='alert alert-danger' role='alert'>
-                                Registration did not work. Please try again!
-                            </div>";
-            }
-        }
-        ?>
-        <form method="POST" action="checkLogin.php" enctype="multipart/form-data">
-            <div class="row">
-                <div class="col-sm-12 input-group input-group-sm mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroup-sizing-sm">Username</span>
+            <!-- Warning messages -->
+            <?php
+                if (isset($_GET['loginDenied'])) {
+                    if ($_GET['loginDenied'] == 'userUnknown') {
+                        echo "  <div class='alert alert-danger' role='alert'>
+                                    User does not exist!
+                                </div>";
+                    } else if ($_GET['loginDenied'] == 'wrongPassword') {
+                        echo "  <div class='alert alert-danger' role='alert'>
+                                    Wrong password!
+                                </div>";
+                    }
+                }
+                if (isset($_GET['registerSuccessful'])) {
+                    if ($_GET['registerSuccessful'] == 'false') {
+                        echo "  <div class='alert alert-danger' role='alert'>
+                                    Registration did not work. Please try again!
+                                </div>";
+                    }
+                }
+            ?>
+
+            <!-- Login form -->
+            <form method="POST" action="checkLogin.php" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col-sm-12 input-group input-group-sm mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-sm">Username</span>
+                        </div>
+                        <input type="text" class="form-control" name="username" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required>
                     </div>
-                    <input type="text" class="form-control" name="username" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-12 input-group input-group-sm mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroup-sizing-sm">Password</span>
+                <div class="row">
+                    <div class="col-sm-12 input-group input-group-sm mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-sm">Password</span>
+                        </div>
+                        <input type="password" class="form-control" name="password" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required>
                     </div>
-                    <input type="password" class="form-control" name="password" aria-label="Small" aria-describedby="inputGroup-sizing-sm" required>
                 </div>
-            </div>
-
-            <button class="btn full-width margin-bottom button-login" type="submit" name="submit">Login</button>
-            <div class="padding-bottom">
-                <a class="btn btn-info" role="button" data-toggle="modal" data-target="#exampleModalCenter">First time on our site? Click here!</a>
-            </div>
-        </form>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-
+                <button class="btn full-width margin-bottom button-login" type="submit" name="submit">Login</button>
+                <div class="padding-bottom">
+                    <a class="btn btn-info" role="button" data-toggle="modal" data-target="#exampleModalCenter">First time on our site? Click here!</a>
+                </div>
+            </form>
+        </div>
+    </body>
 </html>
