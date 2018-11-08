20318806bd289d09bfd43bd3b82c1183d6a63c4e
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-08 19:07:13 +0200

    embed info modal

--- InventoryManager/src/common/basicFunctions.php
+++ InventoryManager/src/common/basicFunctions.php
@@ -73,4 +73,75 @@
             }
         }
     }
+
+
+    function checksecondinfo($numero) {
+        // -- INVENTORY ENTRY TABLE : AMOUNT & STATUS -- //
+
+        $db = connectToDB();
+
+        //echo '<span> Hey Jixou </span>';
+        echo '<br>';
+
+        $sql = "SELECT * FROM Inventoryentry";
+        $result = $db->query($sql);
+
+        $totalAmount = 0;
+        $stock = 1;
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                // echo '<span> La mec </span>';
+                // echo '<br>';
+
+                if ($row['ProductNr'] == $numero) {
+                    //4
+                    echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > - Box n°</span>';
+                    echo $stock;
+                    echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold"> : </span>';
+                    echo '<br>';
+
+                    $stock += 1;
+
+                    //5
+                    echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Amount in stock : </span>';
+                    echo $row['Amount'];
+
+                    $totalAmount += $row['Amount'];
+
+                    echo '<br>';
+
+                    //6
+                    echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Status : </span>';
+
+                    /* if( $row[Status] == 0)
+                         {
+
+                         echo '<span> non-opened </span>';
+
+                         }*/
+                    if ($row['Status'] == 1) {
+                        echo '<span> opened </span>';
+                    }
+                    if ($row['Status'] == NULL) {
+                        echo '<span> no relative info </span>';
+                    }
+
+                    echo '<br>';
+
+                    //7
+                    echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Expiring date : </span>';
+                    echo $row['ExpiringDate'];
+
+                    echo '<br>';
+                    echo '<br>';
+                }
+            }
+        }
+        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Total Amount of this product : </span>';
+        echo $totalAmount;
+
+        echo '<br>';
+        echo '<br>';
+    }
 ?>
