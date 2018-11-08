cc5cd4eeda164f92b59d9ad212daa3edf70d309c
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-08 23:55:40 +0200

    redirects from forms management

--- InventoryManager/src/common/manageForms.php
+++ InventoryManager/src/common/manageForms.php
@@ -7,7 +7,7 @@
     $inventory= $_GET['inventory'];
 
     if (isset($_GET['search'])) {
-        redirect("../inventory/inventory.php?inventory=$inventory&searchEntry=$search_entry");
+        redirect("../inventory/inventory.php?inventory=$inventory&search_entry=$search_entry");
     }
     if (isset($_GET['add_new_inventory'])) {
         redirect("../inventory/uploadInventory.php?name_inventory=$name_inventory&description_inventory=$description_inventory");
