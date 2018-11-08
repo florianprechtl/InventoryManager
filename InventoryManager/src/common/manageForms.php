74a1619b8fb39446ab18b2c1016d3c9e14ec2a2c
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-08 23:30:12 +0200

    add forms management

---
+++ InventoryManager/src/common/manageForms.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo "POST:<br><br>" ;
+    print_r($_POST);
+    var_dump($_POST);
+
+    echo "<br><br>GET:<br><br>";
+    print_r($_GET);
+    var_dump($_GET);
+
+    echo "<br><br>";
+
+    echo "success";
+?>
--- InventoryManager/src/inventory/addInventory_Modal.php
+++ InventoryManager/src/inventory/addInventory_Modal.php
@@ -4,37 +4,32 @@
         <!-- Modal content-->
         <div class="modal-content">
 
-
             <!-- Modal header-->
             <div class="modal-header">
                 <h4 class="modal-title">Add new Inventory</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
 
-
-            <!-- Form which contains body (input elements) and footer (submit button) of the modal -->
-            <form id="inventoryEntryForm" method="GET" action="uploadInventory.php" enctype="multipart/form-data">
-
-                <!-- Modal body-->
-                <div class="modal-body">
-                    <div class="form-group" id="content_new_product">
-                        <div class="form-group">
-                            <label for="name_inventory">Inventory name</label><br>
-                            <input class="form-control" name="name_inventory" type="text">
-                        </div>
-                        <div class="form-group shadow-textarea">
-                            <label for="descr_prod_new">Inventory description</label>
-                            <textarea class="form-control z-depth-1" name="descr_prod_new" rows="3" placeholder="Write something here..."></textarea>
-                        </div>
+            <!-- Modal body-->
+            <div class="modal-body">
+                <div class="form-group" id="content_new_product">
+                    <div class="form-group">
+                        <label for="name_inventory">Inventory name</label><br>
+                        <input class="form-control" name="name_inventory" type="text">
+                    </div>
+                    <div class="form-group shadow-textarea">
+                        <label for="descr_prod_new">Inventory description</label>
+                        <textarea class="form-control z-depth-1" name="descr_prod_new" rows="3" placeholder="Write something here..."></textarea>
                     </div>
                 </div>
+            </div>
 
-                <!-- Modal footer-->
-                <div class="modal-footer">
-                    <button type="submit" name="add_new_inventory" class="btn btn-primary">Submit</button>
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-            </form>
+            <!-- Modal footer-->
+            <div class="modal-footer">
+                <button type="submit" name="add_new_inventory" class="btn btn-primary">Submit</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+
         </div>
 
     </div>
