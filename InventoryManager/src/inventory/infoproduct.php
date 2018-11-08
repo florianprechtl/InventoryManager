d1a709360b2650bd9a783c11fa7d255d41d0879a
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-08 18:50:17 +0200

    embed info modal

--- InventoryManager/src/inventory/infoproduct.php
+++ InventoryManager/src/inventory/infoproduct.php
@@ -1,11 +1,4 @@
-<?php
-    include('../common/connectDB.php');
-    //include('../inventory/functionCheckAmount.php');
-
-?>
-
- 
-    <!-- Modal -->
+<!-- Modal -->
 <div class="modal fade" id="exampleModal<?php echo $_POST['inventory_nr']?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
