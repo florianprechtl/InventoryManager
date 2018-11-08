1c8d7361598c59e2aa4cd7e091336a731d4c2d7f
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-08 14:04:03 +0200

    added header_session_start.php in order to always start the session

--- InventoryManager/src/inventory/inventory.php
+++ InventoryManager/src/inventory/inventory.php
@@ -1,31 +1,10 @@
+<html>
+
 <?php
     include('../common/basicFunctions.php');
     includeWithVariables('../common/connectDB.php', null);
+    include('../common/header_session_start.php');
 ?>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
-    <link rel="icon" href="../../favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="../../styles/resetCss.css">
-    <link rel="stylesheet" href="../../styles/styles.css">
-    <link rel="stylesheet" href="../../styles/inventoryStyles.css">
-    <link rel="stylesheet" href="../../additionals/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="../../additionals/croppie/croppie.css">
-    <link rel="stylesheet" href="../../additionals/bootstrap/datepicker/css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-
-    <script src="../../additionals/jquery/jquery-3.3.1.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="../../additionals/bootstrap/js/bootstrap.js"></script>
-    <script src="../../additionals/croppie/croppie.js"></script>
-    <script src="../../additionals/bootstrap/datepicker/js/bootstrap-datepicker.js"></script>
-    <script src="../../scripts/imgCrop.js"></script>
-
-    <title>Inventory Manager</title>
-</head>
 
 <body>
     <div class="container-fluid" style="max-width: 800px; width:100%">
@@ -47,8 +26,6 @@
                             
                      <!--for infoproduct use that -->
                             <?php
-                                session_start();
-
                                 $db = connectToDB();
 
                                 $sql = "SELECT * FROM Inventory join Inventroyusermatrix on inventory.InventoryNr = Inventroyusermatrix.InventroyNr WHERE UserNr = $_SESSION[user_nr]";
