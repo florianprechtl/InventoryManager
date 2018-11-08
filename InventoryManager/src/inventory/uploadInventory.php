885b52eb1c5735d286ed13425611239307141b40
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-09 00:25:43 +0200

    added insert functionality for new inventoryUserMatrix entry

--- InventoryManager/src/inventory/uploadInventory.php
+++ InventoryManager/src/inventory/uploadInventory.php
@@ -2,12 +2,14 @@
     include('../common/connectDB.php');
     include('../common/basicFunctions.php');
 
+    session_start();
+
     $db = connectToDB();
 
     // Variables of the inventory
     $inventory_name = $_GET['inventory_name'];
     $description_inventory = $_GET['description_inventory'];
-    $inventoryNr="";
+    $inventory_nr="";
 
     // Get next autoincrement value that is returned by the function
     $sql = "SHOW TABLE STATUS WHERE name='inventory'";
@@ -15,14 +17,17 @@
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            $inventoryNr = $row['Auto_increment'];
+            $inventory_nr = $row['Auto_increment'];
         }
     }
 
     // Insert of the new inventory
     $sql = "INSERT INTO Inventory (InventoryNr, Name, Description) VALUES (NULL, '$inventory_name', '$description_inventory')";
-
     $db->query($sql);
 
-    redirect("inventory.php?inventory=$inventoryNr");
+    // Insert of userInventoryEntry
+    $sql = "INSERT INTO InventoryUserMatrix (MatrixNr, InventoryNr, UserNr) VALUES (NULL, $inventory_nr, $_SESSION[user_nr])";
+    $db->query($sql);
+
+    redirect("inventory.php?inventory=$inventory_nr");
 ?>
