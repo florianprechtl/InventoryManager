9c413b0e9f9996b0b959d653d3f21defb443a388
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-09 00:43:52 +0200

    tried prepare statements

--- InventoryManager/src/inventory/uploadInventory.php
+++ InventoryManager/src/inventory/uploadInventory.php
@@ -3,31 +3,37 @@
     include('../common/basicFunctions.php');
 
     session_start();
-
     $db = connectToDB();
+
 
     // Variables of the inventory
     $name_inventory = $_GET['name_inventory'];
     $description_inventory = $_GET['description_inventory'];
-    $inventory_nr="";
+    $inventory_nr = "";
+    $user_nr = $_SESSION['user_nr'];
+    $matrix_nr = null;
+
 
     // Get next autoincrement value that is returned by the function
     $sql = "SHOW TABLE STATUS WHERE name='inventory'";
     $result = $db->query($sql);
-
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             $inventory_nr = $row['Auto_increment'];
         }
     }
 
-    // Insert of the new inventory
-    $sql = "INSERT INTO Inventory (InventoryNr, Name, Description) VALUES (NULL, '$name_inventory', '$description_inventory')";
-    $db->query($sql);
+    // Insert of the new inventory with a prepare statement
+    $sql = "INSERT INTO Inventory (InventoryNr, Name, Description) VALUES (?, ?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('iss', $inventory_nr, $name_inventory, $description_inventory);
+    $stmt->execute();
 
-    // Insert of userInventoryEntry
-    $sql = "INSERT INTO InventoryUserMatrix (MatrixNr, InventoryNr, UserNr) VALUES (NULL, $inventory_nr, $_SESSION[user_nr])";
-    $db->query($sql);
+    // Insert of userInventoryEntry with a prepare statement
+    $sql = "INSERT INTO InventoryUserMatrix (MatrixNr, InventoryNr, UserNr) VALUES (?, ?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('iii', $matrix_nr, $name_inventory, $user_nr);
+    $stmt->execute();
 
     redirect("inventory.php?inventory=$inventory_nr");
 ?>
