409185dee302795805b35fd17a38087621e8b91b
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-11-08 19:09:27 +0200

    Update createTable.php

--- InventoryManager/src/common/createTable.php
+++ InventoryManager/src/common/createTable.php
@@ -1,7 +1,21 @@
+<?php
+    include('../connectDB.php');
 
+echo '<span> creation de table people </span>'
+    
+CREATE TABLE People (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255) 
+);
+/*
 CREATE TABLE tablename (
     column1 datatype,
     column2 datatype,
     column3 datatype,
    ....
-);
+);*/
+
+?>
