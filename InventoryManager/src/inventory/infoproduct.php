eeccffd1a81c8d55ea0ab219d2210d74b5814abe
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-08 19:48:49 +0200

    connected inventoryEntry to ModalInfo

--- InventoryManager/src/inventory/infoproduct.php
+++ InventoryManager/src/inventory/infoproduct.php
@@ -1,6 +1,3 @@
-<?php
-    include('../common/basicFunctions.php');
-?>
 <!-- Modal -->
 <div class="modal fade" id="info_modal_<?php echo $inventoryEntryNr ?>" tabindex="-1" role="dialog"
      aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -16,8 +13,82 @@
             <div class="mx-auto">
                 <!-- PRODUCT TABLE : NAME & DESCRIPTION -->
                 <?php
-                    checkfirstinfo($inventoryEntryNr);
-                    checksecondinfo($inventoryEntryNr);
+                $db = connectToDB();
+
+                $sql = "SELECT * FROM Product join Inventoryentry where Product.ProdNr = Inventoryentry.ProductNr";
+                $result = $db->query($sql);
+
+                //Utilisation de Product table :
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Product number reference: </span>';
+                        echo $row['ProdNr'];
+                        echo '<br>';
+
+                        //1
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Product name : </span>';
+                        echo $row['Name'];
+                        echo '<br>';
+                        echo '<br>';
+
+                        //2
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Description : </span>';
+                        echo $row['Description'];
+                        echo '<br>';
+
+                        //3
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Unit : </span>';
+                        echo $row['Unit'];
+                        echo '<br>';
+
+                        //4
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > - Box n°</span>';
+                        echo $stock;
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold"> : </span>';
+                        echo '<br>';
+
+                        $stock += 1;
+
+                        //5
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Amount in stock : </span>';
+                        echo $row['Amount'];
+
+                        $totalAmount += $row['Amount'];
+
+                        echo '<br>';
+
+                        //6
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Status : </span>';
+
+                        /* if( $row[Status] == 0)
+                             {
+
+                             echo '<span> non-opened </span>';
+
+                             }*/
+                        if ($row['Status'] == 1) {
+                            echo '<span> opened </span>';
+                        }
+                        if ($row['Status'] == NULL) {
+                            echo '<span> no relative info </span>';
+                        }
+
+                        echo '<br>';
+
+                        //7
+                        echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Expiring date : </span>';
+                        echo $row['ExpiringDate'];
+
+                        echo '<br>';
+                        echo '<br>';
+                    }
+                }
+                echo '<span style = "color:grey; font-variant: small-caps; font-weight:bold" > Total Amount of this product : </span>';
+                echo $totalAmount;
+
+                echo '<br>';
+                echo '<br>';
+
                 ?>
             </div>
 
