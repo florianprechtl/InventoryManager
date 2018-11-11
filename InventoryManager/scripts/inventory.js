1735122166b8916e27d0b8cd138e86e81f49c48d
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-12 00:37:00 +0200

    bugfix

--- InventoryManager/scripts/inventory.js
+++ InventoryManager/scripts/inventory.js
@@ -22,15 +22,15 @@ $(document).ready(function() {
 
     $('#button_fade_to_new_product').click(function () {
         $a = $('#content_existing_product');
-        $('#content_existing_product').slideUp(750, function () {
+        $('#content_existing_product').slideToggle(750, function () {
             $b = $('#content_new_product');
-            $('#content_new_product').slideDown(750);
+            $('#content_new_product').slideToggle(750);
         });
     });
 
     $('#button_fade_to_existing_product').click(function () {
-        $('#content_new_product').slideUp(750, function () {
-            $('#content_existing_product').slideDown(750);
+        $('#content_new_product').slideToggle(750, function () {
+            $('#content_existing_product').slideToggle(750);
         });
     });
 
