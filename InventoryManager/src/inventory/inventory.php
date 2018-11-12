f6969339519ba0acaf9ffb188d601a4522250abd
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-12 23:13:29 +0200

    alert if no inventory entry

--- InventoryManager/src/inventory/inventory.php
+++ InventoryManager/src/inventory/inventory.php
@@ -189,10 +189,6 @@
                         echo "<div class='inventory-item-preview alert alert-danger'>";
                         echo "<strong>No entries yet!</strong><br><br>Add one by clicking on the plus panel on the left!</div>";
                     }
-                } else {
-                    echo "  <div class='alert alert-danger inventory-item-preview' role='alert'>
-                                No entries so far!
-                            </div>";
                 }
             ?>
         </div>
