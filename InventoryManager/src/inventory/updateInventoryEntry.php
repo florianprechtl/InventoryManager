3de161f19060d21ffbf84c4c79e0d1a5d699f622
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-12 22:36:57 +0200

    adjusted redirect

--- InventoryManager/src/inventory/updateInventoryEntry.php
+++ InventoryManager/src/inventory/updateInventoryEntry.php
@@ -7,11 +7,12 @@
     if (isset($_POST['save_changes'])) {
         // update inventory entry
 
-        $inventory_nr = $_POST['inventory_entry_nr'];
+        $inventory_entry_nr = $_POST['inventory_entry_nr'];
         $amount = $_POST['amount'];
         $unit = $_POST['unit'];
         $expiring_date = $_POST['date_expiring'];
         $buying_date = $_POST['date_buying'];
+        $inventory_nr = $_SESSION['inventory_nr'];
 
 
         $db = connectToDB();
@@ -19,11 +20,11 @@
         // Insert of the new inventory with a prepare statement
         $sql = "Update InventoryEntry Set Amount = ?, Unit = ?, ExpiringDate = ?, BuyingDate = ? Where InventoryEntryNr = ?";
         $stmt = $db->prepare($sql);
-        $stmt->bind_param('isssi', $amount, $unit, $expiring_date, $buying_date, $inventory_nr);
+        $stmt->bind_param('isssi', $amount, $unit, $expiring_date, $buying_date, $inventory_entry_nr);
         $stmt->execute();
-        redirect('inventory.php?updateSuccessful=true');
+        redirect("inventory.php?inventory=$inventory_nr&updateSuccessful=true");
     }
     if (isset($_POST['dismiss_changes'])) {
-        redirect('inventory.php');
+        redirect("inventory.php?inventory=$inventory_nr");
     }
 ?>
