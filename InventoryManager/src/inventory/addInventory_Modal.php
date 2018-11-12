8e4d35bea7b094c3c3ccff890e2e07189761c277
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-13 00:25:35 +0200

    bugfix search button adds new inventory

--- InventoryManager/src/inventory/addInventory_Modal.php
+++ InventoryManager/src/inventory/addInventory_Modal.php
@@ -15,7 +15,7 @@
                 <div class="form-group" id="content_new_inventory">
                     <div class="form-group">
                         <label for="name_inventory">Inventory name</label><br>
-                        <input class="form-control" required name="name_inventory" type="text">
+                        <input class="form-control" name="name_inventory" type="text" required>
                     </div>
                     <div class="form-group shadow-textarea">
                         <label for="description_inventory">Inventory description</label>
