6a291a08d678810867bd4c979bb3976dd655fa45
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-11-13 14:42:18 +0200

    Update ExpiredProducts.php

--- InventoryManager/src/inventory/ExpiredProducts.php
+++ InventoryManager/src/inventory/ExpiredProducts.php
@@ -1,1 +1,8 @@
 <!-- All the expired products shoukd appear as a list on this page to inform the user -->
+<?php
+	include('../common/connectDB.php');
+    include('../common/basicFunctions.php');
+	
+    $db = connectToDB();
+	
+?>
