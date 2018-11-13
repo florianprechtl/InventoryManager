8f0fc623dc0df0a9d982990338ae20d92699896a
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-11-13 11:21:39 +0200

    Update removeInventoryEntry.php

--- InventoryManager/src/inventory/removeInventoryEntry.php
+++ InventoryManager/src/inventory/removeInventoryEntry.php
@@ -11,6 +11,6 @@
     ## delete img from ftp server ##
     ################################
 
-    // does not necessarily make sense to remove pic from ftp server
+    // does not necessarily make sense to remove pic from ftp server because we could use it again later
     $file = "imgUploads/$_POST[imgName]";
 ?>
