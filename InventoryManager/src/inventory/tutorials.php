834317fba9dd389ba28c1d344759e2ee16048755
JixPOYLO <44200992+JixPOYLO@users.noreply.github.com>
2018-11-26 18:10:11 +0200

    Update tutorials.php

--- InventoryManager/src/inventory/tutorials.php
+++ InventoryManager/src/inventory/tutorials.php
@@ -1,2 +1,2 @@
-<!-- here will be all the links to the tutorials videos -->
+<!-- UNUSEFUL here will be all the links to the tutorials videos UNUSEFUL -->
 premier link à mettre : https://www.youtube.com/watch?v=NScwy4vZkN8&feature=youtu.be
