d7df98b648fa712b6961c4ea4853ff44cd85753b
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-11-29 12:44:40 +0200

    debugging

--- InventoryManager/src/inventory/removeInventoryEntry.php
+++ InventoryManager/src/inventory/removeInventoryEntry.php
@@ -8,21 +8,19 @@
     if (isset($_POST['nr'])) {
         $inventoryEntryNr = filter_var($_POST['nr'], FILTER_SANITIZE_NUMBER_INT);
     } else {
+        return 0;
     }
 
 
     if (isset($_POST['imgName'])) {
         $imgName = filter_var($_POST['nr'], FILTER_SANITIZE_NUMBER_INT);
     } else {
+        return 0;
     }
 
     // Delete inventory entry from db
-    $sql = "DELETE FROM InventoryEntry WHERE inventoryEntryNr = ?";
-    $stmt = $db->prepare($sql);
-    $stmt->bind_param('i', $inventoryEntryNr);
-    $stmt->excute();
-//    $sql = "DELETE FROM InventoryEntry WHERE InventoryEntryNr = $inventoryEntryNr";
-//    $db->query($sql);
+    $sql = "DELETE FROM InventoryEntry WHERE InventoryEntryNr = $inventoryEntryNr";
+    $db->query($sql);
 
 
 
