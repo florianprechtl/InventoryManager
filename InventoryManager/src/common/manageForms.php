f8a7f93a3cdb4e7d13fdf5124c5371db95e8d46b
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-12-07 16:28:03 +0200

    Validation and sanitization

--- InventoryManager/src/common/manageForms.php
+++ InventoryManager/src/common/manageForms.php
@@ -4,14 +4,44 @@ This file decides which php file to redirect to -->
 
     include('basicFunctions.php');
 
-    $name_inventory = $_GET['name_inventory'];
-    $description_inventory = $_GET['description_inventory'];
-    $search_entry = $_GET['search_entry'];
-    $inventory= $_GET['inventory'];
+    // Validation and sanitization
+    // Inventory nr
+    if (isset($_GET['add_new_inventory']) && isset($_GET['inventory'])) {
+        $inventory_nr =  filter_var($_GET['inventory'], FILTER_SANITIZE_NUMBER_INT);
+    } else {
+        $inventory_nr = null;
+    }
 
+    // Name
+    if (isset($_GET['add_new_inventory']) && isset($_GET['name_inventory'])) {
+        $name_inventory =  filter_var($_GET['name_inventory'], FILTER_SANITIZE_STRING);
+    } else {
+        $name_inventory = null;
+        redirect("inventory.php?inventory=$inventory_nr&error=errorNotDefined");
+    }
+
+    // Description
+    if (isset($_GET['add_new_inventory']) && isset($_GET['description_inventory'])) {
+        $description_inventory =  filter_var($_GET['description_inventory'], FILTER_SANITIZE_STRING);
+    } else {
+        $description_inventory = null;
+    }
+
+    // Search entry
+    if (isset($_GET['search']) && isset($_GET['search_entry'])) {
+        $search_entry =  filter_var($_GET['search_entry'], FILTER_SANITIZE_STRING);
+    } else {
+        $search_entry = null;
+    }
+
+
+
+
+
+    // manage which submit button was clicked
     if (isset($_GET['search'])) {
-        redirect("../inventory/inventory.php?inventory=$inventory&search_entry=$search_entry");
+        redirect("../inventory/inventory.php?inventory=$inventory_nr&search_entry=$search_entry");
     } else if (isset($_GET['add_new_inventory'])) {
-        redirect("../inventory/uploadInventory.php?inventory=$inventory&name_inventory=$name_inventory&description_inventory=$description_inventory");
+        redirect("../inventory/uploadInventory.php?inventory=$inventory_nr&name_inventory=$name_inventory&description_inventory=$description_inventory");
     }
 ?>
